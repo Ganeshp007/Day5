345539281bd67ad8b5b38c862c6b7d08ebc247be
Ganeshp007 <potdart40.gp@gmail.com>
2022-05-01 00:30:54 +0530

    Added: LeapYear

--- FlipCoin/Program.cs
+++ FlipCoin/Program.cs
@@ -1,39 +1,13 @@
 ﻿// See https://aka.ms/new-console-template for more information
-int loopCount = 1;
-double numFlips = 50;
-double result;
-int heads = 0;
-int tails = 0;
-int heads_count=0;
-int tails_count=0;  
-while (loopCount <= numFlips)
-{ Random obj = new Random();
-    result = obj.NextDouble() * (1-0)+0;
-    result = Math.Round(result, 1);
-    if (result <= 0.5) 
-    {
-        
-        Console.WriteLine("Coin flippped:- Head");
-        heads++;
-    }
-    else 
-    {
-        
-        Console.WriteLine("Coin flipped :- Tails");
-     
-        tails++;
-    }
-    loopCount++;
+
+int year;
+Console.WriteLine("Enter the year:-");
+year = int.Parse(Console.ReadLine());
+if(year%4==0 && year%400==0)
+{
+    Console.WriteLine(year + " is a Leap Year...");
 }
-
-double percentHeads = (heads / numFlips) * 100;
-percentHeads=Math.Round(percentHeads, 2);
-double percentTails = (tails / numFlips) * 100;
-percentTails=Math.Round(percentTails, 2);   
-Console.WriteLine("\nCount :\n");
-Console.WriteLine("Total Head count:- "+heads);
-Console.WriteLine("Total Tails count:- "+tails);
-Console.WriteLine("\nPercentages :\n");
-Console.WriteLine("Percentage of Heads occured:- " + percentHeads + "%");
-Console.WriteLine("Percentage of Tails occured:- " + percentTails + "%");
-
+else
+{
+    Console.WriteLine(year + " is Not Leap Year...");
+}
