263f75f3d1572163a57c23e9c9da65f615b6ab0e
Ganeshp007 <potdart40.gp@gmail.com>
2022-05-03 21:30:59 +0530

    Added: Largest Number

--- FlipCoin/Program.cs
+++ FlipCoin/Program.cs
@@ -1,19 +1,26 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine("---- Welcome to Prime Factor Solution ----\n");
-int N,i;
+Console.WriteLine("---- Findout Largest Number ----\n");
 
-Console.Write("Enter the number to find Prime Factor:- ");
+Console.Write("Enter value of 1st No :- ");
+int x = Convert.ToInt32(Console.ReadLine());
 
-N = Convert.ToInt32(Console.ReadLine());
-for (i = 1; i <= N; i++)
+Console.Write("Enter value of 2nd No :- ");
+int y = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Enter value of 3rd No :- ");
+int z = Convert.ToInt32(Console.ReadLine());
+
+if ((x > y) && (x > z))
 {
-    if (N % i == 0)
-    {
-
-        Console.WriteLine("{0} is the prime factor of {1}", i, N); 
-        //This is how we print result in respective manner using {} 
-    }
+    Console.WriteLine("{0} is largest value", x);
+}
+else if ((y > x) && (y > z))
+{
+    Console.WriteLine("{0} is largest value", y);
+}
+else
+{
+    Console.WriteLine("{0} is largest value", z);
 
 }
-Console.ReadLine();
