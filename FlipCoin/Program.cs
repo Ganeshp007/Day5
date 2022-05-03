886ca47e60573a85369cf6d6f6c1a7f5d0bad6b9
Ganeshp007 <potdart40.gp@gmail.com>
2022-05-03 21:22:37 +0530

    Added: PrimeFactor

--- FlipCoin/Program.cs
+++ FlipCoin/Program.cs
@@ -1,19 +1,19 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-int n;
+Console.WriteLine("---- Welcome to Prime Factor Solution ----\n");
+int N,i;
 
-Console.WriteLine("Enter the power of 2:- ");
-n= int.Parse(Console.ReadLine());
-if(n>30)
-{
-    Console.WriteLine("\nInt datatype range exceeds..!!\nNOTE: Please enter powers of 2 less than 31...");
-    Environment.Exit(0) ;
-}
+Console.Write("Enter the number to find Prime Factor:- ");
 
-for(int i = 1; i <= n; i++)
+N = Convert.ToInt32(Console.ReadLine());
+for (i = 1; i <= N; i++)
 {
-    double result = 0;
-    result = Math.Pow((2), i); //finding power of 2 for current iteration of i
-    Console.WriteLine("2 to the power " + i +" is = "+ result);
-   
+    if (N % i == 0)
+    {
+
+        Console.WriteLine("{0} is the prime factor of {1}", i, N); 
+        //This is how we print result in respective manner using {} 
+    }
+
 }
+Console.ReadLine();
