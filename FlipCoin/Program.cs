4ede00fb6be7c44db0d85a01b5e1c0a0728e2db8
Ganeshp007 <potdart40.gp@gmail.com>
2022-05-03 21:38:01 +0530

    Added: VowelorConsonant

--- FlipCoin/Program.cs
+++ FlipCoin/Program.cs
@@ -1,26 +1,15 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine("---- Findout Largest Number ----\n");
+Console.WriteLine("---- Findout Alphabate is Vowel OR Consonant ----\n");
 
-Console.Write("Enter value of 1st No :- ");
-int x = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter the Alphabate to check :- ");
+char ch = Convert.ToChar(Console.ReadLine());
 
-Console.Write("Enter value of 2nd No :- ");
-int y = Convert.ToInt32(Console.ReadLine());
-
-Console.Write("Enter value of 3rd No :- ");
-int z = Convert.ToInt32(Console.ReadLine());
-
-if ((x > y) && (x > z))
+if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u' || ch == 'A' || ch == 'E' || ch == 'I' || ch == 'O' || ch == 'U')
 {
-    Console.WriteLine("{0} is largest value", x);
+    Console.WriteLine("\nResult :- {0} is Vowel", ch);
 }
-else if ((y > x) && (y > z))
+else if ((ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'z'))
 {
-    Console.WriteLine("{0} is largest value", y);
-}
-else
-{
-    Console.WriteLine("{0} is largest value", z);
-
+    Console.WriteLine("\nResult :- {0} is Consonant", ch);
 }
